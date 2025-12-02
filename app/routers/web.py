87cd7d52f1fbef3269d4f21f61dd4949a4126db1
Mário Costa <mariocostaa6@gmail.com>
2025-12-02 20:28:40 +0000

    style: format profile routes definitions

--- app/routers/web.py
+++ app/routers/web.py
@@ -38,3 +38,6 @@ def update_profile(request: ProfileUpdateRequest, current_user: User = Depends(g
 @router.put("/profile/password/update", name="v1-profile-password-update")
 def update_password(request: ProfilePasswordUpdateRequest, current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     return ProfileController.update_password(current_user, request.old_password, request.password, request.password_confirm, db)
+
+
+#todo
